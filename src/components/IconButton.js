a592cab786ac54d606f8009414f705930410c663
dietmountaindew <ifetayo99@gmail.com>
2021-12-15 17:31:01 +0900

    ChaiwonHwang mainPageStyled
    스타일링을 전부 styled components로 바꿈
    edit 후 리턴 시 오류 보완

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Pressable, StyleSheet, View, Image } from 'react-native';
+//import { Pressable, StyleSheet, View, Image } from 'react-native';
 import { theme } from '../theme';
 import PropTypes from 'prop-types';
 import { images } from '../Images';
+import { TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native'
 
 const IconButton = ({type, onPressOut, id}) => {
     const _onPressOut = () => {
@@ -10,9 +12,9 @@ const IconButton = ({type, onPressOut, id}) => {
     };
 
     return (
-        <Pressable onPressOut={_onPressOut}>
-            <Image source={type} style={iconStyle.icon}/>
-        </Pressable>
+        <TouchableOpacity onPressOut={_onPressOut}>
+            <Icon source={type} />
+        </TouchableOpacity>
     );
 };
 
@@ -20,19 +22,18 @@ IconButton.defaultProps = {
     onPressOut: () => {},
 };
 
-const iconStyle = StyleSheet.create({
-    icon: {
-        tintColor: theme.text,
-        width: 30,
-        height: 30,
-        margin: 10,
-    },
-});
+const Icon = styled.Image`
+    tint-color: ${({theme, completed})=>completed? theme.done:theme.text};
+    width: 30px;
+    height: 30px;
+    margin: 10px;    
+`;
 
 IconButton.propTypes = {
     type: PropTypes.oneOf(Object.values(images)).isRequired,
     onPressOut: PropTypes.func,
     id: PropTypes.string,
+    completed: PropTypes.bool,
 };
 
 export default IconButton;
--- src/styles.js
+++ src/styles.js
@@ -1,28 +1,32 @@
-import {StyleSheet} from 'react-native';
-import {theme} from './theme';
+// //테마 적용을 styled component로 모두 전환함.
+// //styles.js는 삭제해도 괜찮을 듯함.
 
-export const ViewStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: theme.background,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-});
+// import {StyleSheet} from 'react-native';
+// import { styleProps } from 'react-native-web/dist/cjs/modules/forwardedProps';
+// import {theme} from './theme';
 
-export const textStyles = StyleSheet.create({
-    title: {
-        fontSize: 40,
-        fontWeight: '600',
-        color: theme.main,
-        alignItems: 'flex-start',
-        marginTop: 0,
-        marginLeft: 0,
-    },
-});
+// export const ViewStyles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: theme.background,
+//         alignItems: 'center',
+//         justifyContent: 'flex-start',
+//     },
+// });
 
-export const barStyles = StyleSheet.create({
-    statusbar: {
-        backgroundColor: theme.background,
-    },
-});
+// export const textStyles = StyleSheet.create({
+//     title: {
+//         fontSize: 40,
+//         fontWeight: '600',
+//         color: theme.main,
+//         alignItems: 'flex-start',
+//         marginTop: 0,
+//         marginLeft: 0,
+//     },
+// });
+
+// export const barStyles = StyleSheet.create({
+//     statusbar: {
+//         backgroundColor: theme.background,
+//     },
+// });
