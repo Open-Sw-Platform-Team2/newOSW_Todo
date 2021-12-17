47fab44fbf46532f5f92c6ed4a641d8a28e29685
dietmountaindew <ifetayo99@gmail.com>
2021-12-17 22:56:56 +0900

    ChaiwonHwang
    DeleteAll 구현
    다크모드 전환 버튼으로 깔끔하게 처리

--- src/Images.js
+++ src/Images.js
@@ -2,10 +2,14 @@ import UncheckedBox from '../public/outline_check_box_outline_blank_white_24dp.p
 import CheckedBox from '../public/outline_check_box_white_24dp.png';
 import Delete from '../public/outline_delete_white_24dp.png';
 import Edit from '../public/outline_edit_white_24dp.png';
+import DeleteAll from '../public/icons8-delete-list-48.png';
+import ThemeChanger from '../public/day-and-night.png';
 
 export const images = {
     uncompleted: UncheckedBox,
     completed: CheckedBox,
     delete: Delete,
     update: Edit,
+    deleteAll: DeleteAll,
+    themeChange: ThemeChanger,
 };
