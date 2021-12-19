e27f18a51c7a34f7034b000c614f3552ab1bcd41
Soohyeon B <cabbage9963@gmail.com>
2021-12-19 14:41:29 +0900

    마이페이지 login btn nav 연결 안됨

--- src/screens/MyPageScreen.js
+++ src/screens/MyPageScreen.js
@@ -1,50 +1,52 @@
 import * as React from 'react';
-import { Text, View, Image, ScrollView, SafeAreaView, StyleSheet, SafeAreaViewComponent,TouchableOpacity,} from 'react-native';
+import { Text, View, Image, ScrollView, SafeAreaView, StyleSheet, SafeAreaViewComponent,TouchableOpacity,Button,} from 'react-native';
 // import {ScrollView} from "react-native-web";
+import LoginScreen from './Login';
+import SignupScreen from './Signup';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "react-navigation-stack";
+
+
 
 const MyPageScreen = ({navigation}) => {
     return (
         // <SafeAreaView> //회색박스 생기는 에러 발생부분
-            <ScrollView style = {{flex:1, backgroundColor: '#fff'}}
-                        style={styles.container}
-                        contentContainerStyle={{justifyContent: 'center',alignItems: 'center'}}
-                        showsVerticalScrollIndicator={false}>
-                <Text style = {styles.userName}>Ewhayeon</Text>
-                <Text style = {styles.userId}>ewhain@ewhain.net</Text>
-                <View style = {styles.userBtnWrapper}>
-                    {/*Todo: 본인 계정일 때만 뜨도록 변경해야함*/}
-                    <TouchableOpacity style={styles.userBtn}>
-                        <Text style={styles.userBtnTxt}>Edit</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.userBtn}>
-                        <Text style={styles.userBtnTxt}>Log out</Text>
-                    </TouchableOpacity>
+        <ScrollView style = {{flex:1, backgroundColor: '#fff'}}
+                    style={styles.container}
+                    contentContainerStyle={{justifyContent: 'center',alignItems: 'center'}}
+                    showsVerticalScrollIndicator={false}>
+            <Text style = {styles.userName}>Ewhayeon</Text>
+            <Text style = {styles.userId}>ewhain@ewhain.net</Text>
+            <View style = {styles.userBtnWrapper}>
+                {/*Todo: 본인 계정일 때만 뜨도록 변경해야함*/}
+                {/*<Button title="Login" onPress={()=> navigation.navigate('LoginScreen')}/>*/}
+                {/*새로 stack navigation 띄우는 것 안됨*/}
+                <TouchableOpacity style={styles.userBtn} onPress={()=> navigation.navigate('LoginScreen')}>
+                    <Text style={styles.userBtnTxt}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.userBtn}>
+                    <Text style={styles.userBtnTxt}>Share</Text>
+                </TouchableOpacity>
+            </View>
+            {/*  부가정보  */}
+            <View style={styles.userInfoWrapper}>
+                <View style={styles.userInfoItem}>
+                    <Text style={styles.userInfoTitle}>122</Text>
+                    <Text style={styles.userInfoSubTitle}>Todo Items</Text>
                 </View>
-                {/*  부가정보  */}
-                <View style={styles.userInfoWrapper}>
-                    <View style={styles.userInfoItem}>
-                        <Text style={styles.userInfoTitle}>122</Text>
-                        <Text style={styles.userInfoSubTitle}>Todo Items</Text>
-                    </View>
-                    <View style={styles.userInfoItem}>
-                        <Text style={styles.userInfoTitle}>5</Text>
-                        <Text style={styles.userInfoSubTitle}>Friends</Text>
-                    </View>
+                <View style={styles.userInfoItem}>
+                    <Text style={styles.userInfoTitle}>5</Text>
+                    <Text style={styles.userInfoSubTitle}>Friends</Text>
                 </View>
+            </View>
 
-            </ScrollView>
+        </ScrollView>
         // </SafeAreaView>
-    )
+    );
 }
 
 export default MyPageScreen;
-// {
-//   // return (
-//   //   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//   //     <Text>마이 페이지 작업</Text>
-//   //   </View>
-//   // );
-// }
+
 
 //사용할 스타일
 const styles = StyleSheet.create({
